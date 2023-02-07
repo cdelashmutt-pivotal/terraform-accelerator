dee9bfba9875e9e9344f1124ced47f83bf839bd3
Chris DeLashmutt <cdelashmutt@vmware.com>
2023-02-07 14:45:19 -0500

    Commit

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "resource_group_name" {
 output "kubernetes_cluster_name" {
   value = azurerm_kubernetes_cluster.default.name
 }
+
+output "azure_container_registry_login" {
+  value = "az acr login -n ${azurerm_container_registry.default.name}"
+}
